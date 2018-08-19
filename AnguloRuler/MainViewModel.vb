3d9657cb6798a5195b5b0157c51f2ccbafd51f86
王宇逸 <Strawberry_Str@hotmail.com>
2018-08-19 14:48:02 +0800

    使用ViewModel

---
+++ AnguloRuler/MainViewModel.vb
@@ -0,0 +1,59 @@
+﻿Public Class MainViewModel
+    Inherits DependencyObject
+
+    Public Shared ReadOnly PlayingProperty As DependencyProperty = DependencyProperty.Register(NameOf(Playing), GetType(Boolean), GetType(MainViewModel), New PropertyMetadata(False, AddressOf PlayingChangedCallback))
+    Public Property Playing As Boolean
+        Get
+            Return GetValue(PlayingProperty)
+        End Get
+        Set(value As Boolean)
+            SetValue(PlayingProperty, value)
+        End Set
+    End Property
+    Private Shared Sub PlayingChangedCallback(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
+        Dim model As MainViewModel = d
+        model.OnPlayingChanged(e.NewValue)
+    End Sub
+    Public Event PlayingChanged As EventHandler(Of Boolean)
+    Protected Sub OnPlayingChanged(e As Boolean)
+        RaiseEvent PlayingChanged(Me, e)
+    End Sub
+
+    Public Shared ReadOnly BackImagePathProperty As DependencyProperty = DependencyProperty.Register(NameOf(BackImagePath), GetType(String), GetType(MainViewModel))
+    Public Property BackImagePath As String
+        Get
+            Return GetValue(BackImagePathProperty)
+        End Get
+        Set(value As String)
+            SetValue(BackImagePathProperty, value)
+        End Set
+    End Property
+
+    Public Shared ReadOnly BackVideoPathProperty As DependencyProperty = DependencyProperty.Register(NameOf(BackVideoPath), GetType(String), GetType(MainViewModel))
+    Public Property BackVideoPath As String
+        Get
+            Return GetValue(BackVideoPathProperty)
+        End Get
+        Set(value As String)
+            SetValue(BackVideoPathProperty, value)
+        End Set
+    End Property
+
+    Public Shared ReadOnly PositionProperty As DependencyProperty = DependencyProperty.Register(NameOf(Position), GetType(TimeSpan), GetType(MainViewModel), New PropertyMetadata(TimeSpan.Zero, AddressOf PositionChangedCallback))
+    Public Property Position As TimeSpan
+        Get
+            Return GetValue(PositionProperty)
+        End Get
+        Set(value As TimeSpan)
+            SetValue(PositionProperty, value)
+        End Set
+    End Property
+    Private Shared Sub PositionChangedCallback(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
+        Dim model As MainViewModel = d
+        model.OnPositionChanged(e.NewValue)
+    End Sub
+    Public Event PositionChanged As EventHandler(Of TimeSpan)
+    Protected Sub OnPositionChanged(e As TimeSpan)
+        RaiseEvent PositionChanged(Me, e)
+    End Sub
+End Class
--- AnguloRuler/MainWindow.xaml.vb
+++ AnguloRuler/MainWindow.xaml.vb
@@ -6,96 +6,6 @@ Class MainWindow
     Private openVideo As OpenFileDialog
     Private WithEvents TimerVideo As DispatcherTimer
 
-    Friend Property Playing As Boolean
-        Get
-            Return GetValue(PlayingProperty)
-        End Get
-        Set(value As Boolean)
-            SetValue(PlayingProperty, value)
-        End Set
-    End Property
-
-    Friend Property BackImagePath As String
-        Get
-            Return GetValue(BackImagePathProperty)
-        End Get
-        Set(value As String)
-            SetValue(BackImagePathProperty, value)
-        End Set
-    End Property
-
-    Friend Property BackVideoPath As String
-        Get
-            Return GetValue(BackVideoPathProperty)
-        End Get
-        Set(value As String)
-            SetValue(BackVideoPathProperty, value)
-            If value Is Nothing Then
-                Playing = False
-                ButtonPause.Visibility = Visibility.Hidden
-            Else
-                Playing = True
-            End If
-        End Set
-    End Property
-
-    Friend Property Theta As Double
-        Get
-            Return GetValue(ThetaProperty)
-        End Get
-        Set(value As Double)
-            SetValue(ThetaProperty, value)
-        End Set
-    End Property
-
-    Friend Property Length As Double
-        Get
-            Return GetValue(LengthProperty)
-        End Get
-        Set(value As Double)
-            SetValue(LengthProperty, value)
-        End Set
-    End Property
-
-    Friend Property Position As TimeSpan
-        Get
-            Return GetValue(PositionProperty)
-        End Get
-        Set(value As TimeSpan)
-            SetValue(PositionProperty, value)
-        End Set
-    End Property
-
-    Public Shared ReadOnly PlayingProperty As DependencyProperty = DependencyProperty.Register(NameOf(Playing), GetType(Boolean), GetType(MainWindow), New PropertyMetadata(False, AddressOf PlayingChangeCallback))
-    Public Shared ReadOnly BackImagePathProperty As DependencyProperty = DependencyProperty.Register(NameOf(BackImagePath), GetType(String), GetType(MainWindow))
-    Public Shared ReadOnly BackVideoPathProperty As DependencyProperty = DependencyProperty.Register(NameOf(BackVideoPath), GetType(String), GetType(MainWindow))
-    Public Shared ReadOnly ThetaProperty As DependencyProperty = DependencyProperty.Register(NameOf(Theta), GetType(Double), GetType(MainWindow))
-    Public Shared ReadOnly LengthProperty As DependencyProperty = DependencyProperty.Register(NameOf(Length), GetType(Double), GetType(MainWindow))
-    Public Shared ReadOnly PositionProperty As DependencyProperty = DependencyProperty.Register(NameOf(Position), GetType(TimeSpan), GetType(MainWindow), New PropertyMetadata(TimeSpan.Zero, AddressOf PositionChangeCallback))
-
-    Private Shared Sub PlayingChangeCallback(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
-        Dim w As MainWindow = TryCast(d, MainWindow)
-        Dim flag As Boolean = e.NewValue
-        If w IsNot Nothing Then
-            If flag Then
-                w.TimerVideo.Start()
-                w.ButtonPause.Content = "暂停"
-                w.ButtonPause.Visibility = Visibility.Visible
-                w.VideoMedia.Play()
-            Else
-                w.TimerVideo.Stop()
-                w.ButtonPause.Content = "播放"
-                w.VideoMedia.Pause()
-            End If
-        End If
-    End Sub
-    Private Shared Sub PositionChangeCallback(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
-        Dim w As MainWindow = TryCast(d, MainWindow)
-        If w IsNot Nothing Then
-            w.VideoMedia.Position = CType(e.NewValue, TimeSpan)
-        End If
-    End Sub
-
     Public Sub New()
         InitializeComponent()
 
@@ -104,36 +14,46 @@ Class MainWindow
         openImage.Title = "打开图片"
 
         openVideo = New OpenFileDialog()
-        openVideo.Filter = "视频文件|*.wmv;*.mov;*.mp4|所有文件|*.*"
+        openVideo.Filter = "视频文件|*.wmv;*.mov;*.mp4;*.mkv|所有文件|*.*"
         openVideo.Title = "打开视频"
 
         TimerVideo = New DispatcherTimer()
         TimerVideo.Interval = TimeSpan.FromMilliseconds(10)
+    End Sub
 
-        ContentGrid.DataContext = Me
+    Private Sub ShowImage(path As String)
+        Model.BackImagePath = path
+        Model.BackVideoPath = Nothing
+        Model.Playing = False
+        ButtonPause.Visibility = Visibility.Hidden
+        SliderVideo.Visibility = Visibility.Hidden
+    End Sub
 
-        Length = 100
-
-        SetBinding(ThetaProperty, New Binding("Angle") With {.ElementName = "Angulo"})
-        SetBinding(LengthProperty, New Binding("Length") With {.ElementName = "Angulo"})
+    Private Sub ShowVideo(path As String)
+        Model.BackImagePath = Nothing
+        Model.BackVideoPath = path
+        Model.Playing = True
+        ButtonPause.Visibility = Visibility.Visible
+        SliderVideo.Visibility = Visibility.Visible
     End Sub
 
     Private Sub ButtonOpenImage_Click(sender As Object, e As RoutedEventArgs) Handles ButtonOpenImage.Click
         If openImage.ShowDialog() Then
-            BackImagePath = openImage.FileName
-            BackVideoPath = Nothing
+            ShowImage(openImage.FileName)
         End If
     End Sub
 
     Private Sub ButtonClear_Click(sender As Object, e As RoutedEventArgs) Handles ButtonClear.Click
-        BackImagePath = Nothing
-        BackVideoPath = Nothing
+        Model.BackImagePath = Nothing
+        Model.BackVideoPath = Nothing
+        Model.Playing = False
+        ButtonPause.Visibility = Visibility.Hidden
+        SliderVideo.Visibility = Visibility.Hidden
     End Sub
 
     Private Sub ButtonOpenVideo_Click(sender As Object, e As RoutedEventArgs) Handles ButtonOpenVideo.Click
         If openVideo.ShowDialog() Then
-            BackVideoPath = openVideo.FileName
-            BackImagePath = Nothing
+            ShowVideo(openVideo.FileName)
         End If
     End Sub
 
@@ -142,11 +62,11 @@ Class MainWindow
     End Sub
 
     Private Sub TimerVideo_Tick(sender As Object, e As EventArgs) Handles TimerVideo.Tick
-        Position = VideoMedia.Position
+        Model.Position = VideoMedia.Position
     End Sub
 
     Private Sub ButtonPause_Click(sender As Object, e As RoutedEventArgs) Handles ButtonPause.Click
-        Playing = Not Playing
+        Model.Playing = Not Model.Playing
     End Sub
 
     Private Sub MainWindow_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
@@ -157,5 +77,21 @@ Class MainWindow
 
     Private Sub MainWindow_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
         Angulo.ForceHideCircle = False
+    End Sub
+
+    Private Sub Model_PlayingChanged(sender As Object, e As Boolean) Handles Model.PlayingChanged
+        If e Then
+            TimerVideo.Start()
+            ButtonPause.Content = "暂停"
+            VideoMedia.Play()
+        Else
+            TimerVideo.Stop()
+            ButtonPause.Content = "播放"
+            VideoMedia.Pause()
+        End If
+    End Sub
+
+    Private Sub Model_PositionChanged(sender As Object, e As TimeSpan) Handles Model.PositionChanged
+        VideoMedia.Position = e
     End Sub
 End Class
